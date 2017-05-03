e188f3b4f2894b801f670e6299926c08a8f12fc8
bthieurmel <bthieurmel@gmail.com>
2017-05-03 17:14:51 +0200

    new flexdasboard

--- R/graph.R
+++ R/graph.R
@@ -9,7 +9,7 @@
 #' @export
 graphFlowBased2D <- function(flowbased, ctry1, ctry2)
 {
-  
+
   if(ctry2 == "NL"){
     ptctry2 <- -rowSums(flowbased$pointsY)
     ptctry2X <- -rowSums(flowbased$pointX)
@@ -17,17 +17,17 @@ graphFlowBased2D <- function(flowbased, ctry1, ctry2)
     ptctry2 <- flowbased$pointsY[[ctry2]]
     ptctry2X <- flowbased$pointX[[ctry2]]
   }
-  
-  res <- data.frame(ctry1 = flowbased$pointsY[[ctry1]], 
+
+  res <- data.frame(ctry1 = flowbased$pointsY[[ctry1]],
                     ctry2 = ptctry2)
   res <- res[chull(res),]
   res <- rbind(res, res[1,])
-  res2 <- data.frame(ctry1 = flowbased$pointX[[ctry1]], 
+  res2 <- data.frame(ctry1 = flowbased$pointX[[ctry1]],
                      ctry2 = ptctry2X)
   res2 <- res2[chull(res2),]
   res2 <- rbind(res2, res2[1,])
 
-  
+
   max_r <- max(nrow(res), nrow(res2))
   if(nrow(res)<max_r){
     res <- rbind(res, data.frame(ctry1 = rep(NA, max_r-nrow(res)),
@@ -37,21 +37,21 @@ graphFlowBased2D <- function(flowbased, ctry1, ctry2)
     res2 <- rbind(res2, data.frame(ctry1 = rep(NA,max_r- nrow(res2)),
                                  ctry2 = rep(NA, max_r-nrow(res2))))
   }
-  
+
   out <- cbind(res, res2)
   names(out) <- c("Mctry1", "Mctry2", "Rctry1", "Rctry2")
-  
+
   out <- round(out, 2)
-  
-  
+
+
   pipeR::pipeline(
     amXYChart(dataProvider = out),
-    addGraph(title = "Modélisé", balloonText = 
+    addGraph(title = "Modélisé", balloonText =
                paste0('<b>Modélisé<br>', ctry1, '</b> :[[x]] <br><b>',ctry2, '</b> :[[y]]'),
-             
+
              bullet = 'circle', xField = 'Mctry1',yField = 'Mctry2',
              lineAlpha = 1, bulletAlpha = 0),
-    addGraph(title = "Réel",balloonText =    
+    addGraph(title = "Réel",balloonText =
                paste0('<b>Réel<br>', ctry1, '</b> :[[x]] <br><b>',ctry2, '</b> :[[y]]'),
              bullet = 'circle', xField = 'Rctry1',yField = 'Rctry2',
              lineAlpha = 1, bulletAlpha = 0),
@@ -60,35 +60,36 @@ graphFlowBased2D <- function(flowbased, ctry1, ctry2)
     addValueAxes(title = ctry2, position = "bottom", minimum = -7000, maximum = 7000, unit = " MW"),
     setExport(enabled = TRUE),
     setLegend(enabled = TRUE)
-    
+
   )
-  
+
 }
 
 #' Generate html report
 #'
 #' @param allFB \code{list}, object obtain which computeFB function
-#' @param hour \code{numeric}, hour
 #' @param dayType \code{numeric}, dayType
 #'
-#' @import rmarkdown
-#' @import flexdashboard
-#' 
+#' @import rmarkdown flexdashboard rAmCharts manipulateWidget
+#'
+#' @examples
+#'
+#' \dontrun{
+#' allFB <- computeFB(dayType = 7)
+#' generateRaportFb(allFB, dayType = 7)
+#' }
 #' @export
-generateRaportFb <- function(allFB, hour, dayType){
-  hourS <- hour
-  dayTypeS <- dayType
+generateRaportFb <- function(allFB, dayType){
+
   output_file <- getwd()
-  output_file <- paste0(output_file, "/", "FlowBased_H", hour, "DT",dayType, "_", Sys.Date(), ".html")
+  output_file <- paste0(output_file, "/", "FlowBased_DT",dayType, "_", Sys.Date(), ".html")
   e <- environment()
-  e$hour <- hour
   e$dayType <- dayType
-  e$dta <- allFB[hour == hourS & dayType == dayTypeS]$outFlowBased[[1]]
-  
- 
-  
-  
+  e$dta <- allFB[dayType == dayType]
+
   rmarkdown::render(system.file("/dev/resumeFBflex.Rmd", package = "antaresFlowbased"),
                     output_file = output_file,
+                    params = list(set_title = paste0("Day type ", dayType, " (generated on ", Sys.Date(), ")")),
                     intermediates_dir = getwd(), envir = e)
 }
+
--- inst/dev/executeGetBandOptim.R
+++ inst/dev/executeGetBandOptim.R
@@ -2,7 +2,7 @@
 # library(data.table)
 # library(pipeR)
 # library(ROI)
-# 
+#
 
 #GiB
 # PTDF <- fread("inst/optimWork/PTDF.csv")
@@ -14,19 +14,23 @@
 #                 "D:/Users/titorobe/Desktop/PresolveFB/A.csv", row.names = FALSE, sep = ";")
 #     system("D:/Users/titorobe/Desktop/PresolveFB/EXEC.bat")
 #     tt <- read.table("FiltrageDe_A.csv", sep = "@")
-#     write.table(as.matrix(tt[(which(tt == 'SOMMETS')+1):nrow(tt),]), "tp.csv", 
+#     write.table(as.matrix(tt[(which(tt == 'SOMMETS')+1):nrow(tt),]), "tp.csv",
 #                 row.names = FALSE, col.names = FALSE, quote = FALSE)
 #     data.table(Id_day = X, Period = Y,   fread("tp.csv"))
 #   }, simplify = FALSE)
 # }, simplify = FALSE)
-# 
+#
 # res <- rbindlist(lapply(out, rbindlist))
 # write.table(res,"sommets_which_solver.csv", row.names = FALSE)
-# 
+#
+
+allFB <- computeFB(dayType = 7)
+generateRaportFb(allFB, dayType = 7)
+
 
 
 ##Optim et rapport
-allFB <- cumputeFB(dayType = 8, hour = 12)
+allFB <- computeFB(dayType = 8, hour = 12)
 
 sapply( 1:12, function(X){
   sapply( 1:24, function(Y){
@@ -37,7 +41,7 @@ sapply( 1:12, function(X){
 names(res) <- c("BE", "DE", "FR")
 res <- data.frame(res)
 
-res2 <- data.frame(ctry1 = res[,1], 
+res2 <- data.frame(ctry1 = res[,1],
                    ctry2 = res[,2])
 res2 <- res2[chull(res2),]
 res2 <- rbind(res2, res2[1,])
@@ -55,92 +59,92 @@ pipeR::pipeline(
            bullet = 'circle', xField = 'ctry1',yField = 'ctry2',
            lineAlpha = 1),
   setChartCursor()
-  
+
 )
 
 
 
-# 
-# #Pareto 
-# sum(get_solution(tt, y1_plus[i])$value) + 
+#
+# #Pareto
+# sum(get_solution(tt, y1_plus[i])$value) +
 #   sum(get_solution(tt, y2_plus[i])$value) +
 #   sum(get_solution(tt, y3_plus[i])$value) +
 #   sum(get_solution(tt, y1_moins[i])$value) +
 #   sum(get_solution(tt, y2_moins[i])$value) +
-#   sum(get_solution(tt, y3_moins[i])$value) 
-# 
-# sum(get_solution(tt, x1_plus[i])$value) + 
+#   sum(get_solution(tt, y3_moins[i])$value)
+#
+# sum(get_solution(tt, x1_plus[i])$value) +
 #   sum(get_solution(tt, x2_plus[i])$value) +
 #   sum(get_solution(tt, x3_plus[i])$value) +
 #   sum(get_solution(tt, x1_moins[i])$value) +
 #   sum(get_solution(tt, x2_moins[i])$value) +
-#   sum(get_solution(tt, x3_moins[i])$value) 
-# 
-# 
+#   sum(get_solution(tt, x3_moins[i])$value)
+#
+#
 # #Sommets
 # sommets <- data.frame(y1 = get_solution(tt, y1[i])$value,
 #                       y2 = get_solution(tt, y2[i])$value,
 #                       y3 = get_solution(tt, y3[i])$value)
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # convexData <- function(data){
 #   ch <- chull(data)
-#   data[c(ch, ch[1]), ] 
-#   
+#   data[c(ch, ch[1]), ]
+#
 # }
-# 
+#
 # library(geometry)
 # library(rgl)
-# 
+#
 # all <- rbind(as.matrix(EXTREME_X), as.matrix(sommets))
 # denom <- convhulln(EXTREME_X,option = "FA")$vol + convhulln(sommets,option = "FA")$vol - convhulln(all,option = "FA")$vol
 # 1-denom/convhulln(EXTREME_X,option = "FA")$vol
 # 1-denom/convhulln(sommets,option = "FA")$vol
-# 
-# 
-# 
+#
+#
+#
 # library(hypervolume)
 # vol <- expectation_convex(data.frame(EXTREME_X), check_memory = FALSE)
 # vol2 <- expectation_convex(data.frame(sommets), check_memory = FALSE)
 # hypervolume_holes(vol, vol2)
-# 
-# 
+#
+#
 # EXTREME_X <- data.frame(EXTREME_X)
 # ts.EXTREME_X<- t(convhulln(EXTREME_X))  # see the qhull documentations for the options
-# ## Not run: 
+# ## Not run:
 # rgl.triangles(EXTREME_X[ts.EXTREME_X,1],EXTREME_X[ts.EXTREME_X,2],EXTREME_X[ts.EXTREME_X,3],col="blue",alpha=1)
-# 
+#
 # sommets <- data.frame(sommets)
-# ts.sommets<- t(convhulln(sommets))  
+# ts.sommets<- t(convhulln(sommets))
 # rgl.triangles(sommets[ts.sommets,1],sommets[ts.sommets,2],sommets[ts.sommets,3],col="green",alpha=1)
-# 
+#
 # ashape3d()
-# 
+#
 # polygon3d()
 # mesh.diff(mesh.dsphere(cbind(sommets[ts.sommets,1],sommets[ts.sommets,2],sommets[ts.sommets,3])),
 #           mesh.dsphere(cbind(EXTREME_X[ts.EXTREME_X,1],EXTREME_X[ts.EXTREME_X,2],EXTREME_X[ts.EXTREME_X,3])))
-# 
-# 
+#
+#
 # mesh.dsphere(cbind(sommets[ts.sommets,1],sommets[ts.sommets,2],sommets[ts.sommets,3]))
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
 # convhulln(EXTREME_X)
 # convex.sommet <- cbind(sommets[ts.sommets,1],sommets[ts.sommets,2],sommets[ts.sommets,3])
 # convex.EXTREME_X <- cbind(EXTREME_X[ts.EXTREME_X,1],EXTREME_X[ts.EXTREME_X,2],EXTREME_X[ts.EXTREME_X,3])
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # p = Polygon( convexData(sommets[, c(1,2)]))
 # ps = Polygons(list(p),1)
 # sps = SpatialPolygons(list(ps))
 # plot(sps, col = "blue")
-# 
+#
 # p = Polygon( convexData(EXTREME_X[, c(1,2)]))
 # ps = Polygons(list(p),1)
 # sps2 = SpatialPolygons(list(ps))
@@ -149,4 +153,4 @@ pipeR::pipeline(
 # plot(sps3, col = "green", add = TRUE)
 # sps3@polygons[[1]]@Polygons[[1]]@area/sps2@polygons[[1]]@Polygons[[1]]@area
 # sps3@polygons[[1]]@Polygons[[1]]@area/sps@polygons[[1]]@Polygons[[1]]@area
-# 
+#
