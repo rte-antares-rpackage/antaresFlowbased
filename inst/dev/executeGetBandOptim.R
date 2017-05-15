f258cc46e7428c2e61e41be125154bc4f87a4108
TitouanRobert <titouan.robert.pro@gmail.com>
2017-05-15 09:23:54 +0200

    change bp 2017 files

--- inst/dev/executeGetBandOptim.R
+++ inst/dev/executeGetBandOptim.R
@@ -2,7 +2,7 @@
 
 ##Optim et rapport
 computeFB(PTDF = system.file("/input/ptdf/PTDF.csv", package = "antaresFlowbased"),
-          outputName = "D:/Users/titorobe/Desktop/model1", verbose = 0, nbFaces = 36,dayType = 1)
+          outputName = "D:/Users/titorobe/Desktop/model1", verbose = 0, nbFaces = 36)
 
 addChroniquesFile("D:/Users/titorobe/Desktop/model1")
 setFlowbasedPath(path = "D:/Users/titorobe/Desktop/model1")
