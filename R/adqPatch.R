06de65ad444dd624b248de3302ded2fbe6cdcb9f
LI Marion <marilixx@gr0vsdma.rte-france.com>
2017-10-18 13:28:08 +0200

    test

--- R/adqPatch.R
+++ R/adqPatch.R
@@ -1,4 +1,5 @@
 #' Compute adqPatch for antares study
+#' @title 
 #' 
 #' @param opts \code{list} of simulation parameters returned by the function \link{setSimulationPath}. Defaut to \code{antaresRead::simOptions()}
 #' @param mcYears \code{numeric} include mcYears. Default all (all mcYears are load)
