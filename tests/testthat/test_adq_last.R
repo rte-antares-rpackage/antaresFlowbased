cca7b7be736b078dea3902f26d8f91c0c0609720
Titouan Robert <tr@datak.fr>
2018-02-09 16:40:35 +0100

    #21

--- tests/testthat/test_adq_last.R
+++ tests/testthat/test_adq_last.R
@@ -1,34 +1,36 @@
-context("adq")
+context("adq + strategic reserved")
 
 
 test_that("adq strategic reserved", {
-  area <- fread(system.file("ADQSTRATMRG/new_area.csv", package = "antaresFlowbased"))
-  link <- fread(system.file("ADQSTRATMRG/new_links.csv", package = "antaresFlowbased"))
+  
+  
+  aread <- system.file("ADQSTRATMRG/new_area.csv", package = "antaresFlowbased")
+  if(aread == "")aread <- system.file("inst/ADQSTRATMRG/new_area.csv", package = "antaresFlowbased")
+  linkd <- system.file("ADQSTRATMRG/new_links.csv", package = "antaresFlowbased")
+  if(linkd == "")linkd <- system.file("inst/ADQSTRATMRG/new_links.csv", package = "antaresFlowbased")
+  
+  
+  
+  area <- fread(aread)
+  link <- fread(linkd)
   dta <- list(areas = area, links = link)
-  opts <- list()
-  opts$studyPath <- system.file("testdata/adq/antaresStudy/user/flowbased/ts.txt", package = "antaresFlowbased")
-  opts$studyPath <- gsub("/user/flowbased/ts.txt","" , opts)
+  
   dta$areas <- as.antaresDataTable(dta$areas, timeStep = "hourly", type = "area", synthesis = FALSE)
   dta$links <- as.antaresDataTable(dta$links, timeStep = "hourly", type = "link", synthesis = FALSE)
   
-  adqWhioutStratMrg <- suppressWarnings(.applyAdq(opts = opts, dta))
+  adqWhioutStratMrg <- suppressWarnings(.applyAdq(opts = opts3, dta))
   
   
   areaADQ <- fread(system.file("ADQSTRATMRG/adqpatch_area.csv", package = "antaresFlowbased"))
   linkADQ <- fread(system.file("ADQSTRATMRG/adqpatch_links.csv", package = "antaresFlowbased"))
   
-  
-  
-  
-  
   areaADQ <- as.antaresDataTable(areaADQ, timeStep = "hourly", type = "area", synthesis = FALSE)
   linkADQ <- as.antaresDataTable(linkADQ, timeStep = "hourly", type = "link", synthesis = FALSE)
   
   setkeyv(areaADQ, getIdCols(areaADQ))
+  setkeyv(linkADQ, getIdCols(linkADQ))
+  
   setkeyv(adqWhioutStratMrg$areas, getIdCols(adqWhioutStratMrg$areas))
-  
-  setkeyv(adqWhioutStratMrg$links, getIdCols(adqWhioutStratMrg$links))
-  
   setkeyv(adqWhioutStratMrg$links, getIdCols(adqWhioutStratMrg$links))
   
   
@@ -37,47 +39,145 @@ test_that("adq strategic reserved", {
   expect_true(identical(adqWhioutStratMrg$areas$`UNSP. ENRG`, areaADQ$`UNSP. ENRG`))
   expect_true(identical(adqWhioutStratMrg$areas$LOLD, areaADQ$LOLD))
   expect_true(identical(adqWhioutStratMrg$areas$`DTG MRG`, areaADQ$`DTG MRG`))
+  
+  ##Test link table
+  expect_true(identical(adqWhioutStratMrg$links$`FLOW LIN.`, linkADQ$`FLOW LIN.`))
+  
+  
+  
+  ##Apply strat_reseverd
+  
+  
+  
+  
+  
+  with_mock(readAntares = function(...){
+    beS <- system.file("ADQSTRATMRG/be_strategic.csv", package = "antaresFlowbased")
+    if(beS == "")beS <-  system.file("inst/ADQSTRATMRG/be_strategic.csv", package = "antaresFlowbased")
+    
+    beStrat <- fread(beS, sep = ";")
+    beStrat <- as.antaresDataTable(beStrat, timeStep = "hourly", type = "area", synthesis = FALSE)
+    beStrat$time <- gsub("/", "-", beStrat$time)
+    beStrat$time <- paste0(
+      substr(beStrat$time, 7, 10), "-", 
+      substr(beStrat$time, 4, 5), "-", 
+      substr(beStrat$time, 1, 2), " ", 
+      substr(beStrat$time, 12, 13), ":00:00"
+    )
+    
+    beStrat
+  },
+  {
+    adqStratBe <- suppressWarnings(.applyAdq(opts = opts3, dta, strategic_reserve_be = "toto"))
+    nostrat <- suppressWarnings(.applyAdq(opts = opts3, dta))
+    
+    stratMrgBe <- readAntares()
+    
+    
+    
+    #All reserved > 0
+    expect_true(all(adqStratBe$areas$additionalSR >= 0))
+    
+    
+    rowNotApply <- adqStratBe$areas[LOLD == 0, length(LOLD), by = c("time", "mcYear")][V1 == 4]
+    out <- merge(rowNotApply,  adqStratBe$areas, by = c("time", "mcYear"))
+    
+    #No additional SR when adq not applied
+    expect_true(all(out$additionalSR == 0))
+    
+    ##Area not concern
+    rowSel <- adqStratBe$areas$area != "be"
+    expect_true(all(adqStratBe$areas[rowSel]$additionalSR == 0))
+    
+    
+    #BALANCE & DTG MRG unchanged
+    expect_true(identical(adqStratBe$areas$BALANCE,  nostrat$areas$BALANCE))
+    expect_true(identical(adqStratBe$areas$`DTG MRG`,  nostrat$areas$`DTG MRG`))
+    
+    
+    
+    RowStratM <- merge(adqStratBe$areas[mcYear != 589], stratMrgBe, by = c("area","time", "mcYear"))
+    Notapp <- merge(nostrat$areas[mcYear != 589], stratMrgBe, by = c("area","time", "mcYear"))
+    
+    
+    expect_true(all(ifelse((Notapp$`UNSP. ENRG` - RowStratM$`DTG MRG.y`)<0, 0, (Notapp$`UNSP. ENRG` - RowStratM$`DTG MRG.y`))-
+                      RowStratM$`UNSP. ENRG` == 0))
+    
+    
+    ##Area concern
+    expect_true(sum(adqStratBe$areas[`UNSP. ENRG` == 0]$LOLD) == 0)
+    expect_true(all(adqStratBe$areas[area == "be"]$additionalSR  -(nostrat$areas[area == "be"]$`UNSP. ENRG`-  adqStratBe$areas[area == "be"]$`UNSP. ENRG`) == 0))
+    out <- merge(adqStratBe$areas[area == "be"], stratMrgBe, by = c("mcYear", "time"))
+    expect_true(all(out$additionalSR <= out$`DTG MRG.y`))
+    
+    
+  })
+  
+  
+  
+  
+  with_mock(readAntares = function(...){
+    beS <- system.file("ADQSTRATMRG/de_strategic.csv", package = "antaresFlowbased")
+    if(beS == "")beS <-  system.file("inst/ADQSTRATMRG/de_strategic.csv", package = "antaresFlowbased")
+    
+    beStrat <- fread(beS, sep = ";")
+    beStrat <- as.antaresDataTable(beStrat, timeStep = "hourly", type = "area", synthesis = FALSE)
+    beStrat$time <- gsub("/", "-", beStrat$time)
+    beStrat$time <- paste0(
+      substr(beStrat$time, 7, 10), "-", 
+      substr(beStrat$time, 4, 5), "-", 
+      substr(beStrat$time, 1, 2), " ", 
+      substr(beStrat$time, 12, 13), ":00:00"
+    )
+    
+    beStrat
+  },
+  {
+    adqStratBe <- suppressWarnings(.applyAdq(opts = opts3, dta, strategic_reserve_de = "toto"))
+    nostrat <- suppressWarnings(.applyAdq(opts = opts3, dta))
+    
+    stratMrgBe <- readAntares()
+    
+    #All reserved > 0
+    expect_true(all(adqStratBe$areas$additionalSR >= 0))
+    
+    
+    rowNotApply <- adqStratBe$areas[LOLD == 0, length(LOLD), by = c("time", "mcYear")][V1 == 4]
+    out <- merge(rowNotApply,  adqStratBe$areas, by = c("time", "mcYear"))
+    
+    #No additional SR when adq not applied
+    expect_true(all(out$additionalSR == 0))
+    
+    ##Area not concern
+    rowSel <- adqStratBe$areas$area != "de"
+    expect_true(all(adqStratBe$areas[rowSel]$additionalSR == 0))
+    
+    
+    #BALANCE & DTG MRG unchanged
+    expect_true(identical(adqStratBe$areas$BALANCE,  nostrat$areas$BALANCE))
+    expect_true(identical(adqStratBe$areas$`DTG MRG`,  nostrat$areas$`DTG MRG`))
+    
+    
+    
+    RowStratM <- merge(adqStratBe$areas[mcYear != 589], stratMrgBe, by = c("area","time", "mcYear"))
+    Notapp <- merge(nostrat$areas[mcYear != 589], stratMrgBe, by = c("area","time", "mcYear"))
+    
+    
+    expect_true(all(ifelse((Notapp$`UNSP. ENRG` - RowStratM$`DTG MRG.y`)<0, 0, (Notapp$`UNSP. ENRG` - RowStratM$`DTG MRG.y`))-
+                      RowStratM$`UNSP. ENRG` == 0))
+    
+    
+    ##Area concern
+    expect_true(sum(adqStratBe$areas[`UNSP. ENRG` == 0]$LOLD) == 0)
+    expect_true(all(adqStratBe$areas[area == "de"]$additionalSR  -(nostrat$areas[area == "de"]$`UNSP. ENRG`-  adqStratBe$areas[area == "de"]$`UNSP. ENRG`) == 0))
+    out <- merge(adqStratBe$areas[area == "de"], stratMrgBe, by = c("mcYear", "time"))
+    expect_true(all(out$additionalSR <= out$`DTG MRG.y`))
+    
+    
+  }
+  
+  
+  )
+  
+  
 })
-
-
-##Same for link
-
-
-# 
-# library(ROI)
-# library(data.table)
-# library(antaresRead)
-# opts <- list()
-# opts$studyPath <- system.file("testdata/antaresInput/user/flowbased/ts.txt", package = "antaresFlowbased")
-# opts$studyPath <- gsub("/user/flowbased/ts.txt","" , opts)
-# 
-# 
-# dta <- readRDS(system.file("testdata/adq/adqReserv/adq.RDS", package = "antaresFlowbased"))
-# 
-# 
-# stategicBE <- readRDS(system.file("testdata/adq/adqReserv/stategicBE.RDS", package = "antaresFlowbased"))
-# stategicDE <- readRDS(system.file("testdata/adq/adqReserv/stategicDE.RDS", package = "antaresFlowbased"))
-# 
-# dta <- .applyAdq(opts = opts, dta,
-#                  stategicBE = stategicBE,
-#                  stategicDE = stategicDE)
-# 
-# # fwrite(dta$areas, "D:/Users/titorobe/Desktop/Antares/antaresFlowbased/inst/testdata/adq/adqReserv/outputAreas.csv", sep = ";")
-# # fwrite(dta$links, "D:/Users/titorobe/Desktop/Antares/antaresFlowbased/inst/testdata/adq/adqReserv/outputLinks.csv", sep = ";")
-# 
-# 
-# outArea <- fread(system.file("testdata/adq/adqReserv/outputAreas.csv", package = "antaresFlowbased"))
-# outLink <- fread(system.file("testdata/adq/adqReserv/outputLinks.csv", package = "antaresFlowbased"))
-# 
-# 
-# areaAll <- merge(outArea, dta$areas, by = c("mcYear", "timeId", "area"))
-# linkAll <- merge(outLink, dta$links, by = c("mcYear", "timeId", "link"))
-#
-# expect_equal(all(all(areaAll$BALANCE.x == areaAll$BALANCE.y),
-#                  all(areaAll$`UNSP. ENRG.x` == areaAll$`UNSP. ENRG.y`),
-#                  all(areaAll$LOLD.x == areaAll$LOLD.y),
-#                  all(areaAll$`DTG MRG.x` == areaAll$`DTG MRG.y`),
-#                  all(areaAll$strategicMargin.x == areaAll$strategicMargin.y),
-#                  all(linkAll$`FLOW LIN..x` == linkAll$`FLOW LIN..y`)), TRUE)
-
-# rm(list=ls())
