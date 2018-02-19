fdd376d210955f88499e19cc650745875fa39b32
TitouanRobert <titouan.robert.pro@gmail.com>
2018-02-19 13:56:40 +0100

    add test

---
+++ tests/testthat/test_computeFB.R
@@ -0,0 +1,14 @@
+context("computeFB")
+
+
+test_that("computeFB",{
+  allFB <- computeFB(dayType = 7, hour = 1, reports = FALSE, outputName = temp_dir)
+  
+  expect_true(file.exists(paste0(allFB, "/second_member.txt")))
+  expect_true(file.exists(paste0(allFB, "/weight.txt")))
+  expect_true(file.exists(paste0(allFB, "/domainesFB.RDS")))
+  
+  
+})
+
+
--- tests/testthat/test_graph.R
+++ tests/testthat/test_graph.R
@@ -5,3 +5,29 @@ test_that("test graph", {
   res <- plotFB(1,1,"FR","NL")
   expect_true("combineWidgets" %in% class(res))
 })
+
+
+test_that("test positionViz", {
+
+
+  dta <- antaresRead::readAntares(areas = c("fr", "be", "de", "nl"), 
+                                  links = c("be - de","be - fr","be - nl",
+                                            "de - fr","de - nl"), mcYears = 2,
+                                  select = c("LOLD", "UNSP. ENRG", 
+                                             "DTG MRG", "UNSP. ENRG", "BALANCE", "FLOW LIN."),
+                                  opts = testSt )
+  
+  ## plot a domain and the matching output points 
+ res <- positionViz(opts = testSt, 
+              data = dta,
+              dayType = 1, hour =19:20, 
+              ctry1 = "BE", ctry2 = "FR")
+  
+  
+  expect_true("AmChart" %in% class(res))
+})
+
+
+
+
+ 
