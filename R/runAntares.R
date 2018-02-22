42bb42197d7a137841ef9f3b50872474233b10f6
TitouanRobert <titouan.robert.pro@gmail.com>
2018-02-22 14:04:07 +0100

    remove run antares

--- R/runAntares.R
+++
@@ -1,8 +0,0 @@
-#' Run antares function
-#'
-#' @param cmd \code{character} command launch
-#'
-#' @noRd
-.runAntares <- function(cmd){
-  system(cmd, show.output.on.console = FALSE, intern = TRUE)
-}
