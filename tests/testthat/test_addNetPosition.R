6d9a587edd99b41250ba3b125649c12c27fb8b95
Titouan Robert <tr@datak.fr>
2018-03-12 16:52:50 +0100

    lock opts for addNetPosition

--- tests/testthat/test_addNetPosition.R
+++ tests/testthat/test_addNetPosition.R
@@ -2,8 +2,8 @@ context("addNetPosition")
 
 test_that("computeFB",{
 
-  data <- readAntares(area = "all", links = "all", mcYears = 2)
-  data <- addNetPosition(data, antaresRead::simOptions(), ADQ = FALSE)
+  data <- readAntares(area = "all", links = "all", mcYears = 2, opts = testSt)
+  data <- addNetPosition(data, opts = testSt, ADQ = FALSE)
   ipn1 <- data$areas[!is.na(Balance_CWE)]
   ipn12 <- melt(giveIpn( data$links), id = 1:2)
   expect_true(identical(ipn1$Balance_CWE,ipn12$value))
