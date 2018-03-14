f1cd5de464712c390cefaa9d8c2e67b012bad50b
TitouanRobert <titouan.robert.pro@gmail.com>
2018-03-14 15:00:07 +0100

    delete fr

--- inst/doc/Francais.R
+++
@@ -1,6 +0,0 @@
-## ---- eval=FALSE---------------------------------------------------------
-#  antaresRead::setSimulationPath("D:/exemple_test", 0)
-#  
-#  # initialisation de l'étude flowbased
-#  initFlowBased()
-
